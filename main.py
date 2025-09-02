ca4596df8346d5ddd6ce2958e56f7dc405fcf660
David T Harris <davidtcharris@gmail.com>
2025-09-02 07:56:15 -0400

    Ch2:Data Analysis, L4: Count Words
    Count Words
    
    Time for our first bit of data analysis!
    Let's count the words in the book.
    
    Assignment
    
    - Write a new function that accepts the text from the book as a string,
      and returns the number of words in the string. The .split() method
      will be helpful here.
    - Instead of printing the books contents, print this message
      to the console:
    
      {num_words} words found in the document
    
    Replace {num_words} with the actual number of words in the book.
    
    Run and submit the CLI tests.
    
    --------------
    90% My solution
    
    The main difference is that I did originally return the f
    string from word_count. I changed this when I noticed Boot.dev's
    solution didn't do this and was like "duh, of course we should
    just return the word count from `def word count` of course"

--- main.py
+++ main.py
@@ -1,3 +1,19 @@
+#import re
+
+def word_count(text):
+    #words = re.split(r'\W+', text)
+    #words = re.split(r"[ \n]+", text)
+    #words = re.split(r"[\s]+", text)
+    # This works because we know the last element is blank.
+    # You can't rely on this with unknown text though so not a good solution
+    #words = words[:-1:] # slicing lists: start, stop, step
+
+    words = text.split()
+    #print(words)
+    num_words = len(words)
+    return num_words
+
+
 def get_book_text(filepath):
     # f is a file object
     with open(filepath) as f:
@@ -5,6 +21,9 @@ def get_book_text(filepath):
         return file_contents
 
 def main():
-    print(get_book_text("./books/frankenstein.txt"))
+    book_text = get_book_text("./books/frankenstein.txt")
+    #print(book_text)
+    num_words = word_count(book_text)
+    print(f"{num_words} words found in the document")
 
 main()
