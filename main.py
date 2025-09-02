c89fe120754c01805b0a7e6f466e0139c781eac7
David T Harris <davidtcharris@gmail.com>
2025-09-02 08:01:13 -0400

    Ch2:Data Analysis, L4: Count Words (cleanup)
    - Remove unneeded & commented out code
    - Update book path to not use ./ since it's not needed.

--- main.py
+++ main.py
@@ -1,15 +1,5 @@
-#import re
-
 def word_count(text):
-    #words = re.split(r'\W+', text)
-    #words = re.split(r"[ \n]+", text)
-    #words = re.split(r"[\s]+", text)
-    # This works because we know the last element is blank.
-    # You can't rely on this with unknown text though so not a good solution
-    #words = words[:-1:] # slicing lists: start, stop, step
-
     words = text.split()
-    #print(words)
     num_words = len(words)
     return num_words
 
@@ -21,8 +11,7 @@ def get_book_text(filepath):
         return file_contents
 
 def main():
-    book_text = get_book_text("./books/frankenstein.txt")
-    #print(book_text)
+    book_text = get_book_text("books/frankenstein.txt")
     num_words = word_count(book_text)
     print(f"{num_words} words found in the document")
 
