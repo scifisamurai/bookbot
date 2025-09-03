4362e684fde150815bfbf9bfa88d093ba6b5e726
David T Harris <davidtcharris@gmail.com>
2025-09-02 20:45:23 -0400

    Ch2: Data Analysis, L6: Count Characters (Boot's)
    Boot.dev's solution more or less matched mine. The main
    difference was that they utilized `key in dictionary` to
    see if a key existed in the dictionary.
    
    Hence, update the code to leverage this since this seems more
    pythonic than my previous code for checking this.
    
    Also:
      Add link to `lower` documentation from the Boot.dev lesson.

--- stats.py
+++ stats.py
@@ -1,11 +1,15 @@
 def character_count(text):
     letters = {}
     for raw_char in text:
+        # https://docs.python.org/3/library/stdtypes.html#str.lower
         char = raw_char.lower()
-        if letters.get(char) == None:
-            letters[char] = 1
-        else:
+        # As per the following documentation you can use
+        # `in` to see if a key is in a dictionary.
+        # https://docs.python.org/3/tutorial/datastructures.html#dictionaries
+        if char in letters:
             letters[char] += 1
+        else:
+            letters[char] = 1
 
     return letters
 
