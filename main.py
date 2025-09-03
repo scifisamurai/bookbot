ed48044ff62a7fa2e53e9c530179e492d71cb5d0
David T Harris <davidtcharris@gmail.com>
2025-09-03 14:41:49 -0400

    Ch3: Report, L1: Print a Report (my fix)
    Assignment
    
    This is the structure of the report you need to print:
    
    ============ BOOKBOT ============
    Analyzing book found at books/frankenstein.txt...
    ----------- Word Count ----------
    Found 75767 total words
    --------- Character Count -------
    e: 44538
    t: 29493
    a: 25894
    o: 24494
    i: 23927
    n: 23643
    s: 20360
    r: 20079
    h: 19176
    d: 16318
    l: 12306
    m: 10206
    u: 10111
    c: 9011
    f: 8451
    y: 7756
    w: 7450
    p: 5952
    g: 5795
    b: 4868
    v: 3737
    k: 1661
    x: 691
    j: 497
    q: 325
    z: 235
    æ: 28
    â: 8
    ê: 7
    ë: 2
    ô: 1
    ============= END ===============
    
    - Add a new function to your stats.py file that takes the
      dictionary of characters and their counts and
      returns a sorted list of dictionaries.
    
      - Each dictionary should have two key-value pairs:
        - one for the character itself and
        - one for that character's count (e.g. {"char": "b", "num": 4868}).
    
      - Use the .sort() method:
        https://docs.python.org/3/library/stdtypes.html#list.sort
        - Use a helper function to return
          the "num" key of each dictionary for comparison.
        - Sort the list from greatest to least by the count.
    
    - Import and call the function in main.py, and capture the result.
    
    - Print the report to the console as shown above.
      If the character is not an alphabetical character
      (using the .isalpha() method), just skip it.
      https://docs.python.org/3/library/stdtypes.html#str.isalpha
    
      (Be sure to match the expected output in the CLI tests!)
    
    Run and submit the CLI tests.

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from stats import word_count, character_count
+from stats import word_count, character_count, sorted_character_counts
 
 def get_book_text(filepath):
     # f is a file object
@@ -7,11 +7,22 @@ def get_book_text(filepath):
         return file_contents
 
 def main():
+    print("============ BOOKBOT ============")
+    print("Analyzing book found at books/frankenstein.txt...")
     book_text = get_book_text("books/frankenstein.txt")
+
+    print("----------- Word Count ----------")
     num_words = word_count(book_text)
-    print(f"{num_words} words found in the document")
+    print(f"Found {num_words} total words")
+
+    print("--------- Character Count -------")
     num_chars = character_count(book_text)
     #num_chars = character_count("hello")
-    print(num_chars)
+    #print(num_chars)
+    pairs = sorted_character_counts(num_chars)
+    for pair in pairs:
+        if pair["char"].isalpha():
+            print(f"{pair["char"]}: {pair["num"]}")
+    print("============= END ===============")
 
 main()
--- stats.py
+++ stats.py
@@ -1,3 +1,29 @@
+# unsorted_character_counts: dictionary of characters and their counts
+# Example:
+#  { "b": 3, "a": 5 }
+#
+# @returns:
+#   a sorted list of dictionaries sorted by `num` descending.
+#   https://docs.python.org/3/library/stdtypes.html#list.sort
+# Example:
+# [
+#   { "char": "a", "num": 5},
+#   { "char": "b", "num": 3}
+# ]
+def sorted_character_counts(unsorted_character_counts):
+    pairs = []
+    # .items() returns a tuple so `pair` is a tuple
+    # https://www.w3schools.com/python/ref_dictionary_items.asp
+    for raw_pair in unsorted_character_counts.items():
+        pairs.append({"char": raw_pair[0], "num": raw_pair[1]})
+
+    pairs.sort(reverse=True, key=sort_on)
+
+    return pairs
+
+def sort_on(items):
+    return items["num"]
+
 def character_count(text):
     letters = {}
     for raw_char in text:
