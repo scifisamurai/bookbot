59bff273748011c09e083be4387a1492853d2e03
David T Harris <davidtcharris@gmail.com>
2025-09-02 20:03:41 -0400

    Ch2: Data Analysis, L6: Count Characters (my fix)
    Count Characters
    
    Ah, the meat of bookbot: counting characters!
    
    Assignment
    
    - Add a new function to your stats.py file that takes the text
      from the book as a string, and returns the number of times
      each character, (including symbols and spaces), appears in the string.
    
    - Convert any character to lowercase using the .lower() method, we don't
      want duplicates.  Use a dictionary of String -> Integer. The
      returned dictionary should look something like this:
    
    ```python
    {'p': 6121, 'r': 20818, 'o': 25225, ...
    ```
    
    - Import and call the function in main.py, and
      capture the result in a new variable.
    - After printing the word count, print
      the dictionary of characters and their counts.
    
    Run and submit the CLI tests.
    
    -----
    My solution

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from stats import word_count
+from stats import word_count, character_count
 
 def get_book_text(filepath):
     # f is a file object
@@ -10,5 +10,8 @@ def main():
     book_text = get_book_text("books/frankenstein.txt")
     num_words = word_count(book_text)
     print(f"{num_words} words found in the document")
+    num_chars = character_count(book_text)
+    #num_chars = character_count("hello")
+    print(num_chars)
 
 main()
--- stats.py
+++ stats.py
@@ -1,3 +1,14 @@
+def character_count(text):
+    letters = {}
+    for raw_char in text:
+        char = raw_char.lower()
+        if letters.get(char) == None:
+            letters[char] = 1
+        else:
+            letters[char] += 1
+
+    return letters
+
 def word_count(text):
     words = text.split()
     num_words = len(words)
